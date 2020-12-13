255ec3a9154c6f0789b144ac674efeeec6ee875c
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-12-13 23:49:22 +0700

    Replace Counter function into Hello World string generation function to mimic Lab-1 app functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Hello World'),
@@ -28,15 +19,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,35 +26,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   String _helloName = "World";
 
-  void _incrementCounter() {
+  void _updateHelloName(String name) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-      _helloName = _counter.toString();
+      _helloName = 'Hello, $name!';
     });
+  }
+
+  final nameInputController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameInputController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-        backgroundColor: Color(0xff008577),
+        backgroundColor: Color(0xFF008577),
       ),
       body: Column(
         children: <Widget>[
@@ -81,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
               const SizedBox(width: 16,),
               Expanded(
                 child: TextField(
+                  controller: nameInputController,
                   decoration: InputDecoration(
                     border: UnderlineInputBorder(),
                     hintText: 'Enter new name',
@@ -89,7 +65,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               const SizedBox(width: 16,),
               RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _updateHelloName(nameInputController.text);
+                },
                 child: Text(
                   "ENTER",
                   style: Theme.of(context).textTheme.body2,
@@ -100,20 +78,18 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Expanded(
             child: Center(
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
               child: Text(
-                "Hello, World!"
+                _helloName
               ),
             ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: Icon(Icons.help_outline),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
